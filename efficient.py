d586e28d3354b0c83ba3ffaa54b0f69fb689b1f3
Xinxiang Chen <139855390+xinxiang-chen@users.noreply.github.com>
2025-11-09 15:47:43 -0800

    fixed: integrate string generator into one file (#3)
    * feat: generate sequence
    
    * feat: add comments
    
    * fixed: integrate string generator into one file

---
+++ efficient.py
@@ -0,0 +1,46 @@
+import sys
+
+def generate(file: str) -> list:
+    """ Generate the sequence from input files
+
+    Args:
+        file (str): file path
+
+    Returns:
+        list: 2 generated substring, [seq1, seq2]
+    """
+
+    # Read file and store data as a map: subsequence -> [insert position]
+    seq_dict = {}
+    with open(file) as f:
+        subseq = ""
+        insert_position = []
+        for line in f:
+            line = line.strip()
+            try:
+                line = int(line)
+                insert_position.append(line)
+            except:
+                if subseq != "":
+                    seq_dict[subseq] = insert_position
+                    insert_position = []
+                subseq = line
+        seq_dict[subseq] = insert_position
+    
+    # Build sequence
+    ret = []
+    for k, v in seq_dict.items():
+        seq = k     # base
+        for i in v:
+            seq = seq[:i+1] + seq + seq[i+1:]
+        ret.append(seq)
+    return ret
+
+
+if __name__ == "__main__":
+    pass
+    # if len(sys.argv) != 3:
+    #     print("Usage: python3 efficient.py input_path output_path", file=sys.stderr)
+    #     sys.exit(2)
+    # _, input_path, output_path = sys.argv
+    # generate(input_path)
