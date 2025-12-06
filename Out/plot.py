2e8ccc45526b85d837d04483ebf3f88c678dbc5d
Xinxiang Chen <139855390+xinxiang-chen@users.noreply.github.com>
2025-12-05 21:31:54 -0800

    feat: add experiment results (#8)
    * run experiment and get all results
    
    * draw graphs
    
    * fix: integrate strgen.py into basic and efficient (#7)
    
    * add git ignore
    
    * fix: datapoints error

---
+++ Out/plot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main(drawMem=False):
+    # Read csv
+    df = pd.read_csv("Out/datapoints_table.csv")
+
+    # Check columns (optional sanity print)
+    # print(df.head())
+
+    x = df["size"]
+    y_basic = df["time_basic"]
+    y_effi = df["time_effi"]
+    label = 'CPU Time (ms)'
+
+    if drawMem:
+        y_basic = df["mem_basic"]
+        y_effi = df["mem_effi"]
+        label = 'Memory (KB)'
+
+    plt.figure(figsize=(10,6))
+
+    # size vs time_basic
+    plt.plot(x, y_basic, marker="o", linestyle="-", label="basic")
+
+    # size vs time_effi
+    plt.plot(x, y_effi, marker="o", linestyle="-", label="efficient")
+
+    plt.xlabel("Problem Size")
+    plt.ylabel(label)
+    plt.title(f"Problem size vs {label}")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig('Out/memory.png')
+
+if __name__ == "__main__":
+    main(drawMem=True)
